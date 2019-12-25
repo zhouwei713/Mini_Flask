6b6d445f46df780d1f8597810ee2f67d5b5cd2b9
zhouwei713 <736552883@qq.com>
2019-12-25 15:12:05 +0800

    update .gitignore

--- Server/app/db_opera/__init__.py
+++
@@ -1,6 +0,0 @@
-# coding = utf-8
-"""
-@author: zhou
-@time:2019/12/12 20:47
-@File: __init__.py.py
-"""
--- Server/app/tools/__init__.py
+++
@@ -1,6 +0,0 @@
-# coding = utf-8
-"""
-@author: zhou
-@time:2019/12/19 16:00
-@File: __init__.py.py
-"""
